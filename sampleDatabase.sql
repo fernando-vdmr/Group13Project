eaded0c8f20bf4c71486fcfdcb99dab4793dbdaa
chri9449 <chri9449@colorado.edu>
2017-03-13 20:47:39 -0600

    Chad-Added some sample data to the database

--- sampleDatabase.sql
+++ sampleDatabase.sql
@@ -2,8 +2,21 @@ drop table users;
 drop table dogs;
 drop table profilePicture;
 
-create table users (id integer primary key, userName char(20), dogBreed char(20), owner boolean not null default 0);
+create table users (id integer primary key, userName char(20), password char(20), dogBreed char(20), owner boolean not null default 0);
 create table dogs (name char(20), amount integer);
 create table profilePicture (id integer references users(id), picturePath char(20));
 
+insert into users values (1,'Chad', 'password', 'Siberian Husky', true);
 
+insert into dogs values ('Alaskan Malamute',7);
+insert into dogs values ('Australian Shepherd',5);
+insert into dogs values ('Beagle',12);
+insert into dogs values ('Collie',1);
+insert into dogs values ('Labrador Retriever',8);
+insert into dogs values ('Mastiff',2);
+insert into dogs values ('Pomeranian',5);
+insert into dogs values ('Poodle',1);
+insert into dogs values ('Pug',20);
+insert into dogs values ('Siberian Husky',6);
+insert into dogs values ('St. Bernard',4);
+insert into dogs values ('Yorkshire Terrier',1);
